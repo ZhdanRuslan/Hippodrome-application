2fc1073cbb44947246ff13c8c4be33515485de6b
Zhdan Ruslan <zhdanruslan@i.ua>
2016-06-03 22:42:33 +0300

    Hippodrome

---
+++ Hippodrome.java
@@ -0,0 +1,88 @@
+package zhdan.ruslan.hippodrome;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ruslan Zhdan on 27.01.2016.
+ */
+public class Hippodrome
+{
+    private ArrayList<Horse> horses = new ArrayList<>();
+    public static Hippodrome game;
+
+    public ArrayList<Horse> getHorses()
+    {
+        return horses;
+    }
+
+    public static void main(String[] args)
+    {
+        game = new Hippodrome();
+        game.getHorses().add(new Horse("Grom", 3, 0));
+        game.getHorses().add(new Horse("Veter", 3, 0));
+        game.getHorses().add(new Horse("Molnia", 3, 0));
+
+        game.run();
+        game.printWinner();
+    }
+
+    public void run()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            move();
+            print();
+            try
+            {
+                Thread.sleep(500);
+            }
+            catch (InterruptedException e)
+            {
+                /*NOP*/
+            }
+        }
+    }
+
+    public void move()
+    {
+        for (Horse h : this.getHorses())
+        {
+            h.move();
+        }
+    }
+
+    public void print()
+    {
+        for (Horse h : this.getHorses())
+        {
+            h.print();
+            System.out.println("\n");
+        }
+    }
+
+    public Horse getWinner()
+    {
+        Horse winner = null;
+
+        for (Horse h : getHorses())
+        {
+            if (winner == null)
+            {
+                winner = h;
+            } else
+            {
+                if (winner.getDistance() < h.getDistance())
+                {
+                    winner = h;
+                }
+            }
+        }
+        return winner;
+    }
+
+    public void printWinner()
+    {
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
+}
---
+++ Horse.java
@@ -0,0 +1,62 @@
+package zhdan.ruslan.hippodrome;
+
+public class Horse
+{
+    private String name;
+    private double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance)
+    {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed)
+    {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance)
+    {
+        this.distance = distance;
+    }
+
+    public void move()
+    {
+        setSpeed(getSpeed() * Math.random() * 1.2);
+        setDistance(getDistance() + getSpeed());
+    }
+
+    public void print()
+    {
+        String s = "";
+        for (int i = 0; i < distance; i++)
+        {
+            s += ".";
+        }
+        s += getName();
+        System.out.println(s);
+    }
+}
